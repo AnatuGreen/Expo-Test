9fe15957f5dde397753bcd6ab095add69eb5427a
AnatuGreen <anatuonline@gmail.com>
2022-08-20 01:40:20 +0100

    React Native Example codes for  Collapsible and Image carousel based on https://github.com/philipszdavido/react-native-carousel-tut

---
+++ CarouselSample/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    title: "Aenean leo",
+    body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+    imgUrl:
+      "https://images.pexels.com/photos/13252219/pexels-photo-13252219.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  },
+  {
+    title: "In turpis",
+    body: "Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ",
+    imgUrl:
+      "https://images.pexels.com/photos/12640214/pexels-photo-12640214.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  },
+  {
+    title: "Lorem Ipsum",
+    body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
+    imgUrl:
+      "https://images.pexels.com/photos/13219657/pexels-photo-13219657.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  },
+  {
+    title: "Lorem Ipsum",
+    body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
+    imgUrl:
+      "https://images.pexels.com/photos/1872108/pexels-photo-1872108.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  },
+];
+
+export default data;
